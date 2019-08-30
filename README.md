69cedcd15643cdd51b38bbf811a8ef834de9fce6
deazth@gmail.com <deazth@gmail.com>
2019-08-30 14:47:32 +0800

    blank

---
+++ README.md
@@ -0,0 +1,16 @@
+# Kelas PHP 101
+
+1. set configurations kat .env file
+
+2. generate key
+```
+php artisan key:generate
+```
+
+3. create table dan load data
+```
+php artisan migrate --seed
+```
+
+4. test
+
---
+++ database/factories/KerjaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Kerja;
+use Faker\Generator as Faker;
+
+$factory->define(Kerja::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
